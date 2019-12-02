56d4e152c6568cd6d7a00384b5dfd3dfa9544282
dujianwu123 <dujianwu123@126.com>
2019-12-02 17:31:42 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue_bootstrap/dist',
+    assetsPublicPath: '/vue_bootstrap/dist/',
 
     /**
      * Source Maps
